454f105e76ec518ce823a6a9b8bbdc2d007d7d6c
YugiohPhuc <40845465+YugiohPhuc@users.noreply.github.com>
2018-07-05 12:12:42 +0700

    Update Abc.py

--- Abc.py
+++ Abc.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-print ("hello Phuc")
-=======
-<<<<<<< HEAD
-print ("Hello Huy")
-=======
-print ("good bye")
->>>>>>> 21e074f17d670ee4e81361a60566c03f91775407
->>>>>>> bd39eb7779ce19c39db93c065273f07a1a60182b
+print ("hello world")
