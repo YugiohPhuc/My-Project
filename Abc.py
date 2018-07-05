2e7ec6f80d3d28e987428c8a1c811ca47c2654a9
YugiohPhuc <40845465+YugiohPhuc@users.noreply.github.com>
2018-07-05 11:58:39 +0700

    Update Abc.py

--- Abc.py
+++ Abc.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-print('Goodbye Everybody')
-=======
-<<<<<<< HEAD
-print 'goodbye'
-=======
-print('Hello World')
->>>>>>> 5b9789eb64c78b8a6f5c4d56e478c380ac105dd1
->>>>>>> ac11698c7db40e6fed3c8e4a06a187fce997874b
+print ("hello world")
